f3cc91b816d2f6c94ca5df3c57583cb0888e487e
jindorry <ekfaktldk11@gmail.com>
2025-02-16 15:02:51 +0900

    draft Grid view

--- src/clients/components/MainContainer.tsx
+++ src/clients/components/MainContainer.tsx
@@ -1,5 +1,18 @@
-import { Container } from "@mui/material";
+import { Container, Grid2 } from "@mui/material";
+import MainCard from "./MainCard";
+import ImageMasonry from "./ImageMasonry";
 
 export default function MainContainer() {
-  return <Container>Container</Container>;
+  return (
+    <Container sx={{ marginY: "1em" }}>
+      <Grid2 container spacing={2}>
+        <Grid2 size={4}>
+          <MainCard />
+        </Grid2>
+        <Grid2 size={8}>
+          <ImageMasonry />
+        </Grid2>
+      </Grid2>
+    </Container>
+  );
 }
