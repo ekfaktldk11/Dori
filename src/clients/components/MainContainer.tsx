bb1bc93938ef0f5206347e995058c373b8b17d72
jindorry <ekfaktldk11@gmail.com>
2025-02-20 22:11:13 +0900

    modify style - remove scroll from body element - add auto overflow to MainContainer

--- src/clients/components/MainContainer.tsx
+++ src/clients/components/MainContainer.tsx
@@ -4,8 +4,13 @@ import ImageMasonry from "./ImageMasonry";
 
 export default function MainContainer() {
   return (
-    <Container sx={{ marginY: "1em" }}>
-      <Grid2 container spacing={2}>
+    <Container
+      sx={{
+        overflow: "auto",
+        height: "calc(100vh - 3em)", // 3em : Navbar.tsx Container height
+      }}
+    >
+      <Grid2 container spacing={2} sx={{ marginY: "1em" }}>
         <Grid2 size={4}>
           <MainCard />
         </Grid2>
