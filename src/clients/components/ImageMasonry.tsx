628511eab2ba93d6bd385e23f7bfaecd774679f2
jindorry <ekfaktldk11@gmail.com>
2025-02-23 12:45:15 +0900

    refactor: wrap import

--- src/clients/components/ImageMasonry.tsx
+++ src/clients/components/ImageMasonry.tsx
@@ -1,4 +1,4 @@
-import Box from '@mui/material/Box';
+import { Box } from '@mui/material';
 import Masonry from '@mui/lab/Masonry';
 
 export default function ImageMasonry() {
--- src/clients/components/MainCard.tsx
+++ src/clients/components/MainCard.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+import { Box, Card, CardContent, Typography } from "@mui/material";
 
 const card = (
   <React.Fragment>
